b66c1935902b26ee092da2c2aadc070893fefb9c
Marina Morcos <marinamorcos@hotmail.com>
2023-03-03 18:32:41 +0100

    added js and edit html and css

---
+++ script.js
@@ -0,0 +1,27 @@
+function updateDate() {
+  let stockholmElement = document.querySelector("#stockholm");
+  if (stockholmElement) {
+    let stockholmDateElement = stockholmElement.querySelector(".date");
+    let stockholmTimeElement = stockholmElement.querySelector(".time");
+    let stockholmTime = moment().tz("Europe/Stockholm");
+
+    stockholmDateElement.innerHTML = stockholmTime.format("dddd MMMM	Do YYYY");
+    stockholmTimeElement.innerHTML = stockholmTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let cairoElement = document.querySelector("#cairo");
+  if (cairoElement) {
+    let cairoDateElement = cairoElement.querySelector(".date");
+    let cairoTimeElement = cairoElement.querySelector(".time");
+    let cairoTime = moment().tz("Africa/Cairo");
+
+    cairoDateElement.innerHTML = cairoTime.format("dddd MMMM	Do YYYY");
+    cairoTimeElement.innerHTML = cairoTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+updateDate();
+setInterval(updateDate, 1000);
