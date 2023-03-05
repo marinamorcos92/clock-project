5b52d5ae0a4b3749d9606fad5ffb5a3d0c53a4e5
Marina Morcos <marinamorcos@hotmail.com>
2023-03-05 15:05:32 +0100

    fixing issues

--- script.js
+++ script.js
@@ -35,22 +35,22 @@ function updateDate() {
     );
   }
 }
-updateDate();
-setInterval(updateDate, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityName = cityTimeZone.replace("-", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  let cities = document.querySelector("#cities");
-  citiesElement.innerHTML = `<div>
-        <h2>{cityName}</h2>
-        <div class="date">${cityformat("dddd MMMM	Do YYYY")}</div>
+  let cityElement = document.querySelector("#cities");
+  cityElement.innerHTML = `<div class="city">
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("dddd MMMM	Do YYYY")}</div>
         <div class="time">${cityTime.format(
           "hh:mm:ss [<small>]A[</small>]"
         )} </div>
      </div>`;
 }
+updateDate();
+setInterval(updateDate, 1000);
 
 let citySelectElement = document.querySelector("#city");
 citySelectElement.addEventListener("change", updateCity);
