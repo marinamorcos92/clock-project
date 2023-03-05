b69ade0472ef4dee72313e74adf83a19458a9a94
Marina Morcos <marinamorcos@hotmail.com>
2023-03-05 14:46:10 +0100

    update select date and time

--- script.js
+++ script.js
@@ -37,3 +37,20 @@ function updateDate() {
 }
 updateDate();
 setInterval(updateDate, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let cities = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div>
+        <h2>{cityName}</h2>
+        <div class="date">${cityformat("dddd MMMM	Do YYYY")}</div>
+        <div class="time">${cityTime.format(
+          "hh:mm:ss [<small>]A[</small>]"
+        )} </div>
+     </div>`;
+}
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
